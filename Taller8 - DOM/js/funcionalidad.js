a5b141d926302d4b8e32112d38bc3af723224fef
Cliente CONTROL-PC <Cliente CONTROL-PC>
2017-06-12 10:48:02 -0500

    Practica hoy. Flayedra

--- Taller8 - DOM/js/funcionalidad.js
+++ Taller8 - DOM/js/funcionalidad.js
@@ -12,8 +12,13 @@ function porEtiqueta(tag) {
 	console.log(lista);
 
 	/*Mostrar el elementos en la posición 0 y 5*/
-
+	console.log(lista[0])
+	console.log(lista[5])
+	
 	/*Mostrar cada uno de los elementos*/
+	for (var i = 0; i<lista.length ; i++){
+		console.log(lista[i]);
+	}
 
 }
 
@@ -22,11 +27,18 @@ function porNombreDeClase(className) {
 	console.log(lista);
 
 	/*Mostrar cada uno de los elementos*/
+	for (var i = 0; i<lista.length ; i++){
+		console.log(lista[i]);
+	}
 }
 
 function cambiarEstiloImagen() {
 	/*Usando los selectores adecuados, cambie la clase de las imágenes a img-thumbnail*/
-
+	lista = document.querySelectorAll(".caption img")
+	
+	for(let i = 0; i<lista.length; i++){
+		lista[i].setAttribute("class","img-thumbnail")
+	}
 }
 
 function cambiarInnerOutter() {
@@ -41,29 +53,40 @@ function cambiarInnerOutter() {
 
 function agregarPie() {
 	/*crear un footer*/
+	let footer = document.createElement('footer')
 
 	/*Agregar la clase: footer y text-center*/
+	footer.setAttribute("class","footer text-center")
 	
 	/*crear un p*/
-	
+	let p = document.createElement('p')
+
 	/*crear texto: CinemaTu*/
-	
+	let text = document.createTextNode('CinemaTu')
 	/*Agregar texto a p*/
+	p.appendChild(text)
 	
-	/*agregar titulo a footer*/
-	
+	/*agregar p a footer*/
+	footer.appendChild(p)
+
 	/*crear un texto con el año*/
+	let year = document.createTextNode('2017')
 	
 	/*agregar el año al titulo antes del texto de CinemaTu*/
+	p.insertBefore( year ,text)
 	
 	/*agregar el footer a contenedor principal*/
+	let container = document.getElementById('contenedor-principal')
+	container.appendChild(footer)
 	
-	/*reemplazar el innerElement del titulo por 2017 - CinemaTu */
+	/*reemplazar el innerElement del p por 2017 - CinemaTu */
+	p.innerHTML = "2017 - CinemaTu"
 	
 	/*crear un nodo con texto con '2017 - CinemaTu' y reemplazar por titulo*/
+	p.outerHTML = "<p> 2017 - CinemaTu </p>"
 	
 	/*remover el footer*/
-
+	container.removeChild(footer)
 
 }
 
